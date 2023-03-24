5d2bcb954130a5787558787eb704a877134f6752
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-03-24 10:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import sys
-	
-	def perform_operation(num1, num2, operation):
-	    if operation == "add":
-        return num1 + num2
-    elif operation == "sub":
-        return num1 - num2
-    elif operation == "mul":
-        return num1 * num2
-    elif operation == "div":
-	return num1 / num2
-    else:
-        return "Invalid operation"
+def perform_operation(num1, num2, operation):
+	if operation == "add":
+        	return num1 + num2
+	elif operation == "sub":
+        	return num1 - num2
+	elif operation == "mul":
+        	return num1 * num2
+	elif operation == "div":
+		return num1 / num2
+	else:
+        	return "Invalid operation"
 
 # Get input values from command line arguments
 num1 = float(sys.argv[1])
