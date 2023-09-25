d963cc9b4fe31a72c1aceccf5376591c264f64fc
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-09-25 09:09:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import sys
-def perform_operation(num1, num2, operation):
-	if operation == "add":
-        	return num1 + num2
-	elif operation == "sub":
-        	return num1 - num2
-	elif operation == "mul":
-        	return num1 * num2
-	elif operation == "div":
-		return num1 / num2
-	else:
-        	return "Invalid operation"
+import os
 
-# Get input values from command line arguments
-num1 = float(sys.argv[1])
-num2 = float(sys.argv[2])
-operation = sys.argv[3]
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "sub":
+        return num1 - num2
+    elif operation == "mul":
+        return num1 * num2
+    elif operation == "div":
+        return num1 / num2
+    else:
+        return "Invalid operations"
+
+# Get input values from environment variables
+num1 = float(os.environ.get('num1', 0))  # Default value is 0
+num2 = float(os.environ.get('num2', 1))  # Default value is 1 to avoid division by zero
+operation = os.environ.get('operation', 'add')  # Default operation is 'add'
 
 result = perform_operation(num1, num2, operation)
-print("Result:", result)
+print("Result is", result)
